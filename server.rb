a44dffa1f56da838dc939bb8b17f12a34966f358
Stefan <stefan.wienert@pludoni.de>
2012-09-27 10:38:38 +0200

    build with parameters

--- server.rb
+++ server.rb
@@ -9,7 +9,12 @@ post "/" do
   data = JSON.parse request.env["rack.input"].read
   project = CONFIG["projects"][data["ref"]]
   if project
-    url = "http://#{CONFIG["ci_host"]}/job/#{project}/build?token=#{CONFIG["token"]}"
+    if project["build_params"]
+      url = "http://#{CONFIG["ci_host"]}/job/#{project["project"]}/buildWithParameters?token=#{project["token"]}#{project["build_params"]}"
+    else
+      url = "http://#{CONFIG["ci_host"]}/job/#{project["project"]}/build?token=#{project["token"]}#{project["build_params"]}"
+
+    end
     $stdout.puts url
     if auth = CONFIG["basic_auth"]
       # HTTParty needs auth in symbols
@@ -17,6 +22,7 @@ post "/" do
     else
       opts = {}
     end
+    puts "-> GET #{url}"
     HTTParty.get url, opts
   end
   "success"
